15e0ba4e3730c831b7b43701c3f7d8fea24f49f5
BlackArcher <67962548+BlackArcheruz@users.noreply.github.com>
2022-02-06 20:06:38 +0500

    Alpha Release 0.4

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -53,7 +53,7 @@ function Home(){
         .card{
             height: 200px;
             border-radius: 5px;
-            width: 30vw;
+            width: 25vw;
             padding: 1rem;
             margin: 2rem 0;
         }
