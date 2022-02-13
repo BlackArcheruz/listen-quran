061da549a02ab420a02a5dad219c2e3995a8663a
BlackArcherUz <breakingreport4@gmail.com>
2022-02-13 22:11:14 +0500

    Beta Release 0.6

--- src/App.js
+++ src/App.js
@@ -44,9 +44,10 @@ function App() {
   let arrFiltered = arr?.filter(el=>el!==undefined)
   let editions = arrFiltered.map(edition=>edition);
   let [edition,setEdition] = useState('https://server8.mp3quran.net/afs')
-  let Qori = localStorage.getItem('Edition')
+
   const setNewEdition = (e)=>{
     setEdition(e.target.value)
+    let Qori = localStorage.getItem('Edition')
     if(Qori){
       localStorage.setItem('Edition',e.target.value)
     }else{
